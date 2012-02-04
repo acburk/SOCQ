c112ca5befc5892b1aa410707248325abf57ab4a
AC-MBA <adam.burkepile@gmail.com>
2012-02-04 03:07:37 -0800

    Added take

---
+++ SOCQ/SOCQ+NSArray.h
@@ -0,0 +1,15 @@
+//
+//  SOCQ+NSArray.h
+//  SOCQ+NSArray
+//
+//  Created by Adam Burkepile on 2/4/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (SOCQ)
+
+- (NSMutableArray*)take:(int)inCount;
+
+@end
---
+++ SOCQ/SOCQ+NSArray.m
@@ -0,0 +1,18 @@
+//
+//  SOCQ.m
+//  SOCQ
+//
+//  Created by Adam Burkepile on 2/4/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import "SOCQ+NSArray.h"
+
+@implementation NSArray (SOCQ)
+
+- (NSMutableArray*)take:(int)inCount {
+    
+    return nil;
+}
+
+@end
