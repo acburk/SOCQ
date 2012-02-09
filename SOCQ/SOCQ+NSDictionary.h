de75f52c66cadd8b0a347cf1b21a345015397b6d
AC-MBA <adam.burkepile@gmail.com>
2012-02-08 23:33:32 -0800

    added SOCQ+NSDictionary stub class

---
+++ SOCQ/SOCQ+NSDictionary.h
@@ -0,0 +1,14 @@
+//
+//  SOCQ+NSDictionary.h
+//  SOCQ
+//
+//  Created by Adam Burkepile on 2/8/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (SOCQ)
+- (NSDictionary*)where:(BOOL(^)(id key, id value))check;
+
+@end
---
+++ SOCQ/SOCQ+NSDictionary.m
@@ -0,0 +1,17 @@
+//
+//  SOCQ+NSDictionary.m
+//  SOCQ
+//
+//  Created by Adam Burkepile on 2/8/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import "SOCQ+NSDictionary.h"
+
+@implementation NSDictionary (SOCQ)
+
+- (NSDictionary*)where:(BOOL(^)(id key, id value))check {
+    return nil;
+}
+
+@end
