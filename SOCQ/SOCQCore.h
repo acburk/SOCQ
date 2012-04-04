46f3b3724200126a7b98dc06fb19361e57bc9f2f
AC-MBA <adam.burkepile@gmail.com>
2012-04-03 21:39:17 -0700

    initial commit

---
+++ SOCQ/SOCQCore.h
@@ -0,0 +1,12 @@
+//
+//  SOCQCore.h
+//  SOCQ
+//
+//  Created by Adam Burkepile on 4/3/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SOCQCore : NSObject
+@end
---
+++ SOCQ/SOCQCore.m
@@ -0,0 +1,13 @@
+//
+//  SOCQCore.m
+//  SOCQ
+//
+//  Created by Adam Burkepile on 4/3/12.
+//  Copyright (c) 2012 Adam Burkepile. All rights reserved.
+//
+
+#import "SOCQCore.h"
+
+@implementation SOCQCore
+
+@end
