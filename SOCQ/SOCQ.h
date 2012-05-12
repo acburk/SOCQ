667e8eac3416b6eb600dc28949bcfb9f14fb51cb
acburk <adam.burkepile@gmail.com>
2012-05-12 00:02:27 -0700

    added a main header file

---
+++ SOCQ/SOCQ.h
@@ -0,0 +1,38 @@
+//
+//  SOCQ.h
+//  SOCQ
+//
+//  Created by Adam Burkepile on 4/3/12.
+//  Copyright (C) 2012 Adam Burkepile.
+//
+//  Permission is hereby granted, free of charge, to any person 
+//  obtaining a copy of this software and associated documentation 
+//  files (the "Software"), to deal in the Software without 
+//  restriction, including without limitation the rights to use, 
+//  copy, modify, merge, publish, distribute, sublicense, and/or 
+//  sell copies of the Software, and to permit persons to whom 
+//  the Software is furnished to do so, subject to the following 
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be 
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#ifndef SOCQ_SOCQ_h
+#define SOCQ_SOCQ_h
+
+#import "SOCQCore.h"
+#import "SOCQ+NSArray.h"
+#import "SOCQ+NSDictionary.h"
+#import "SOCQ+NSSet.h"
+
+#endif
